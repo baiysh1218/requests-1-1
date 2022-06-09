54d496ac49836816dce3e4e0f5fa92566874b59e
baiysh1218 <baiyshbeksultanov1@gmail.com>
2022-06-09 12:55:24 +0600

    added paginate search updata

--- main.js
+++ main.js
@@ -17,14 +17,12 @@ CRUD - Create(POST) Read(GET) Update(PUT, PATCH) Delete(DELETE)
 const API = "http://localhost:8000/todos";
 let inpAdd = document.getElementById("inp-add");
 let btnAdd = document.getElementById("btn-add");
-// console.log(inpAdd, btnAdd);
 // навесили событие на кнопку "сохранить"
 btnAdd.addEventListener("click", async function () {
   // собрипаем объект для добовление в дб.жейсон
   let newTodo = {
     todo: inpAdd.value,
   };
-  // console.log(newTodo);
   // делаем проверку на заполненность инпута и остонавливаем код c помощю return, чтоб пост-запрос не выполнялся
   if (newTodo.todo.trim() === "") {
     alert("заполните поле");
@@ -45,16 +43,32 @@ btnAdd.addEventListener("click", async function () {
 });
 
 // ! Read
+// ! Search
+let inpSearch = document.getElementById("inp-search");
+inpSearch.addEventListener("input", function () {
+  getTodos();
+});
+
+let pagination = document.getElementById("pagination");
+let page = 1;
+
 // получаем элемент, чтою=б убедится, что в переменной ist сейчас НЕ пусто
 let list = document.getElementById("list");
-// console.log(list);
 // фукция для получения всех тасков и отоброжения их в div#list
 // async await нужен здесь, чтоб при отправке запроса мы сначала получили данные и только потом записали все в переменную response, иначе (если мы НЕ дождемся)туда запищется pending (состояние примиса, который еще не выполнен)
 async function getTodos() {
-  let response = await fetch(API) // если не указать метод запроса, то по умолчанию это GET запрос
+  let response = await fetch(
+    `${API}?q=${inpSearch.value}&_page=${page}&_limit=2`
+  ) // если не указать метод запроса, то по умолчанию это GET запрос
     .then(res => res.json()) // переводим все в json формат
     .catch(err => console.log(err)); // отловили ошибку
-  // console.log(response);
+
+  // allЕodo - все элементы из дб.жейсон
+  let allTodos = await fetch(API)
+    .then(res => res.json())
+    .catch(err => console.log(err));
+  // посчтитали какой будет последняя страница
+  let lastPage = Math.ceil(allTodos.length / 2);
   // очишаем div#list, чтоб список тасков корректно отображался и не хранил там предыдущие html-элементы со старыми данными
   list.innerHTML = "";
   // перебираемт полученный из дб.жсон массива создаем div и задаем ему содержимое через метод innerHTML, каждый созданный элемент аппендим в div#list
@@ -63,20 +77,85 @@ async function getTodos() {
     newElem.id = item.id;
     newElem.innerHTML = `
     <span>${item.todo}</span>
-    <button class = "btn-delete">Delete</button>`;
+    <button class = "btn-delete">Delete</button>
+    <button class = "btn-edit">Edit</button>
+    `;
     list.append(newElem);
   });
+
+  // добовляем погинацию
+  pagination.innerHTML = `
+  <button id="btn-prev" ${page === 1 ? "disabled" : ""}>prev</button>
+  <span>${page}</span>
+  <button ${page === lastPage ? "disabled" : ""} id="btn-next">next</button>
+  `;
 }
 // вызываем функцию , чтоб как только откроется страница что-то было отобразить
 getTodos();
 
+// элементы на модалке для редактирования
+let modalEdit = document.getElementById("modal-edit");
+let modalEditClose = document.getElementById("modal-edit-close");
+
+let inpEditTodo = document.getElementById("inp-edit-todo");
+let inpEditId = document.getElementById("inp-edit-id");
+let btnSaveEdit = document.getElementById("btn-save-edit");
+
+// ф-я для закрытия модалки
+
+modalEditClose.addEventListener("click", function () {
+  // объект с отредактированными данными
+  modalEdit.style.display = "none";
+});
+
+btnSaveEdit.addEventListener("click", async function () {
+  let editedTodo = {
+    todo: inpEditTodo.value,
+  };
+
+  let id = inpEditId.value;
+  // запросы для изменение данных
+  await fetch(`${API}/${id}`, {
+    method: "PATCH", // указываем метод
+    body: JSON.stringify(editedTodo), // указываем что именно нужно запостить
+    headers: {
+      "Content-type": "application/json; charset=utf-8",
+    }, //кодировка
+  });
+  // после изменения закрываем модалку для эдит
+  modalEdit.style.display = "none";
+  getTodos();
+});
+
 document.addEventListener("click", async function (e) {
+  //! DELETE
   if (e.target.className === "btn-delete") {
+    // запросы для удаления
     let id = e.target.parentNode.id;
     await fetch(`${API}/${id}`, {
       method: "DELETE",
     });
     getTodos();
   }
-  // console.log(e.target.parentNode.id);
+  //! UPDATA (edit)
+  if (e.target.className === "btn-edit") {
+    modalEdit.style.display = "flex";
+    let id = e.target.parentNode.id;
+    // запросы для получения данных чтоб мы могли отоброзить все в модалке для релактирования
+    let response = await fetch(`${API}/${id}`)
+      .then(res => res.json())
+      .catch(err => console.log(err));
+    // полученные данные отоброжаем в инпутах из хтмл
+    inpEditTodo.value = response.todo;
+    inpEditId.value = response.id;
+  }
+  // ! Pagination
+  if (e.target.id === "btn-next") {
+    page++;
+    getTodos();
+  }
+  if (e.target.id === "btn-prev") {
+    page--;
+    getTodos();
+  }
 });
